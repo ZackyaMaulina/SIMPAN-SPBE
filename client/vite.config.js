7a3b7a862ed9b2822fb534f348a754bbb53a7422
Masriadi <dev.masriadi@gmail.com>
2023-01-11 13:36:19 +0700

    Membersihkan komentar pada viteConfig

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,6 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
-// import { resolve } from 'path'
-// import { defineConfig } from 'vite'
-
 export default defineConfig({
   build: {
     rollupOptions: {
