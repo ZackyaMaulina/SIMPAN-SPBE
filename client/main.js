58bf3688e43e0ce33ff4a5fb3cadd34a312faa57
levitawahyuni <wahyunilevita@gmail.com>
2023-01-24 14:27:58 +0700

    new

--- client/main.js
+++ client/main.js
@@ -1,1 +1,19 @@
 import './style.scss';
+
+let body = document.body;
+let showModal = document.querySelector('.add-article');
+let closeModal = document.querySelector('.btn-close');
+let modal = document.querySelector('.modal');
+
+
+showModal.addEventListener("click", function (e) {
+    e.preventDefault();
+    modal.classList.toggle('show');
+    body.style.overflow = "hidden";
+});
+closeModal.addEventListener("click", function (e) {
+    e.preventDefault();
+    modal.classList.toggle('show');
+    body.style.overflow = "auto";
+});
+
